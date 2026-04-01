8bbe4b96e88b23f0578367eedb8b1d100f0c247c
WORK <WORK@Sunjays-Mini.modem>
2026-04-01 20:32:26 +1100

    Fix route conflict: use path='/*' for protected routes so LandingPage at / is not shadowed

--- workspace/src/App.jsx
+++ workspace/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route
-            path="/"
+            path="/*"
             element={
               <ProtectedRoute>
                 <Layout />
